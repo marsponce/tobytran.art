9b9fa2ff07e92b866e2dc09e0c38697a91fdfec5
Mars Ponce <mars@marsponce.ca>
2026-01-15 16:37:10 -0500

    Styled index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,25 @@ import * as React from "react";
 
 const IndexPage = () => {
   return (
-    <main>
-      <h1>tobytran.art</h1>
+    <main className="min-h-screen flex items-center justify-center">
+      <div className="text-center">
+      <h1 className="text-9xl">
+        tobytran.art
+      </h1>
+      <br/>
       <p>
         this site is currently under construction.
       </p>
+      <p>
+        please direct any questions to <a
+          className="italic"
+          href="mailto://info@tobytran.art"
+        >
+          info@tobytran.art
+        </a>
+        .
+      </p>
+      </div>
     </main>
   )
 }
