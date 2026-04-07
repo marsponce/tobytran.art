d2ac2f0149ab65ccd0d4fd30ecfce0a667efa18c
Mars Ponce <mars@marsponce.ca>
2026-04-07 16:45:45 -0400

    chore: brought back wip text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,17 @@
 
 export default function Index() {
   return (
-    <main>
-      <div style={{ height: '50vh' }} />
+    <main className="wip">
+      <h1>tobytran.art</h1>
+      <br />
+      <p>This site is currently under construction.</p>
+      <p>
+        please direct any questions to{' '}
+        <a className="italic" href="mailto://info@tobytran.art">
+          info@tobytran.art
+        </a>
+        .
+      </p>
     </main>
   );
 }
