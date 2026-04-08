209046126cd1c6049520ab5823bb614ec3ed5d99
Mars Ponce <mars@marsponce.ca>
2026-04-08 16:41:40 -0400

    chore: replace each page with Wip component

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,18 +1,6 @@
-// /src/app/page.tsx
+// /src/app/contact/page.tsx
+import WipPage from '@/components/pages/Wip';
 
-export default function Index() {
-  return (
-    <main className="wip">
-      <h1>tobytran.art</h1>
-      <br />
-      <p>This site is currently under construction.</p>
-      <p>
-        please direct any questions to{' '}
-        <a className="italic" href="mailto://info@tobytran.art">
-          info@tobytran.art
-        </a>
-        .
-      </p>
-    </main>
-  );
+export default function Contact() {
+  return <WipPage />;
 }
