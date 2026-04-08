7d5b9532b942e65806a0b1b42a991b9986794f36
Mars Ponce <mars@marsponce.ca>
2026-04-08 17:48:40 -0400

    chore: add className to HeaderProps, organize Navigation.module.css

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,8 +1,9 @@
 // src/app/components/Header.tsx
 type HeaderProps = {
   children?: React.ReactNode;
+  className?: string;
 };
 
-export default function Header({ children }: HeaderProps) {
-  return <header>{children}</header>;
+export default function Header({ children, className }: HeaderProps) {
+  return <header className={className ?? ''}>{children}</header>;
 }
