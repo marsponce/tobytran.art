c47996c35788dc2ec498f68f9246e3c839324986
Mars Ponce <mars@marsponce.ca>
2026-04-15 16:59:28 -0400

    feat: integrate decap cms

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,17 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   experimental: {
     viewTransition: true,
   },
-};
-
-module.exports = {
   allowedDevOrigins: ['192.168.1.23', '192.168.0.19'],
+  redirects: async () => [
+    {
+      source: '/admin',
+      destination: '/admin/index.html',
+      permanent: false,
+    },
+  ],
 };
 
 export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import '@/styles/globals.css';
 
 export const metadata: Metadata = {
@@ -17,6 +18,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>{children}</body>
+      <Script
+        src="https://identity.netlify.com/v1/netlify-identity-widget.js"
+        strategy="afterInteractive"
+      />
     </html>
   );
 }
